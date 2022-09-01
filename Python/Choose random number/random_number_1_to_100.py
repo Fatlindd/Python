d4bc0c57dbd7d5df03039a7e580b1d4ab835e334
Fatlindd <48216389+Fatlindd@users.noreply.github.com>
2022-09-01 10:09:19 +0200

    10 random numrat per ushtrime

--- Python/Choose random number/random_number_1_to_100.py
+++ Python/Choose random number/random_number_1_to_100.py
@@ -1,1 +1,21 @@
 
+import random
+
+print('\n')
+def check():
+    with open("detyra.txt") as f:
+        lista = [int(x) for x in f.read().split()]
+
+    return lista
+
+for i in range(1, 11):
+    num = random.randint(1, 100)
+
+    while num in check():
+        num = random.randint(1, 100)
+    else:
+        f = open('detyra.txt', 'a')
+        f.write("{} ".format(num))
+        f.close()
+
+print('Numrat jane gjeneruar! Shiko ne fajlin detyra.txt!')
