b4e9134deb13a61a5f4410d12573c309406d4cfe
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2022-12-12 20:16:25 +0100

    Create a dictionary with 10 questions on it with keys and values.

---
+++ simpleQuiz.py
@@ -0,0 +1,14 @@
+
+questions = {
+    '1. What does CPU stand for?': 'central processing unit',
+    '2. What does RAM stand for?': 'random access memory',
+    '3. What does GPU stand for?': 'graphics processing unit',
+    '4. What does PSU stand for?': 'power supply',
+    '5. Which extensions used for Python?': 'py',
+    '6. What extensions do the text files have?': 'txt',
+    '7. What does OOP mean?': 'Object Oriented Programming',
+    '8. What does it mean ROM?': 'read-only memory',
+    '9. What does it mean SSD storage?': 'solid state drive',
+    '10. What does it mean hdd storage?': 'hard disk drive'
+}
+
