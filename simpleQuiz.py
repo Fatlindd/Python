9b433c74172ae3e22442ad1e16fa885eda37c73f
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2022-12-12 20:19:52 +0100

    If statement to ask user if he or she wants to play, and also create two variables to count correct and incorrect answer.

--- simpleQuiz.py
+++ simpleQuiz.py
@@ -6,9 +6,17 @@ questions = {
     '4. What does PSU stand for?': 'power supply',
     '5. Which extensions used for Python?': 'py',
     '6. What extensions do the text files have?': 'txt',
-    '7. What does OOP mean?': 'Object Oriented Programming',
+    '7. What does OOP mean?': 'object oriented programming',
     '8. What does it mean ROM?': 'read-only memory',
     '9. What does it mean SSD storage?': 'solid state drive',
     '10. What does it mean hdd storage?': 'hard disk drive'
 }
+
+play = input('>| Do you want to play(Y/N)? ')
+if play.lower() == 'n':
+    quit
+else:   
+    correct = 0
+    incorrect = 0
+
 
