fd6ad9169959713675e5368ac845a6caf3e7fa91
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2022-12-13 17:51:58 +0100

    creating a random list with certain operations

---
+++ programWithListElements.py
@@ -0,0 +1,18 @@
+
+listNumbers = [22, 41, 12, 18, 34, -1, 25, 16, 11]
+print('>| Choose one option: ' + 
+        '\n 1. Get largest number' +
+        '\n 2. Get second largest number'+
+        '\n 3. Get smallest number' +
+        '\n 4. Get second smallest number' +
+        '\n 5. Sort list' +
+        '\n 6. Get reversed list' +
+        '\n 7. Get even and odd numbers' +
+        '\n 8. Get sum of numbers in list' +
+        '\n 9. Remove specific element from list' +
+        '\n 10. Add new element in list' + 
+        '\n 11. Check if specific number is in list' +
+        '\n 12. How many elements are in list' +
+        '\n 13. Save the list in a file named numbers.txt' +
+        '\n 14. Clear list and generate new one' +
+        '\n 15. Print list')
