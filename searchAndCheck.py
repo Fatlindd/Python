bf8653dc2c98bbc5fc0dc9fbf2eb859788d54f37
Fatlind <fatlind.thaqi98@gmail.com>
2022-12-14 12:37:04 +0100

    Created search function to find index of number

--- searchAndCheck.py
+++ searchAndCheck.py
@@ -1,4 +1,10 @@
 
+def search(arr, N, x):
+    for i in range(0, N):
+        if arr[i] == x:
+            return i
+    return -1
+
 arr = [2, 3, 4, 5, 10, 40, 24, 15, 32, 4, 13, 10]
 x = int(input('> Search number: '))
 N = len(arr)
