a85663b82a1c991b4ded50f4f3d934fc56715a43
Fatlind <fatlind.thaqi98@gmail.com>
2022-12-14 12:42:27 +0100

    Created checkForDuplicate function, to check if number that you've searched is duplicated on list or not

--- searchAndCheck.py
+++ searchAndCheck.py
@@ -5,6 +5,18 @@ def search(arr, N, x):
             return i
     return -1
 
+def checkForDuplicate(arr, num):
+    sortedList = arr
+    sortedList.sort()
+    count = 0
+    for element in sortedList:
+        if element == num:
+            count += 1
+    
+    if count > 1:
+        return True
+    return False
+
 arr = [2, 3, 4, 5, 10, 40, 24, 15, 32, 4, 13, 10]
 x = int(input('> Search number: '))
 N = len(arr)
