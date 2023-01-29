b52062c2f463afb18694feb19f68377682ddd043
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2023-01-29 14:17:37 +0100

    __init__() method with one parameter PIN

--- BankAccount.py
+++ BankAccount.py
@@ -2,3 +2,9 @@
 class BankAccount:
     print('!!! To Put Your Card In ATM Press 1 !!!')
     user_input = input('Enter card: ')
+
+    def __init__(self, PIN):
+        self.__balance = 1000
+        self.pincode = PIN
+    
+    
