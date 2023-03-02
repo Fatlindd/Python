5bb56d6e1c9fc3a208a093a87405a62d124832a2
Fatlind <48216389+Fatlindd@users.noreply.github.com>
2023-03-02 11:56:41 +0100

    Adding a method check_in() with two parameters, which control if room_number is occupied or not.

--- hotel.py
+++ hotel.py
@@ -7,3 +7,12 @@ class Hotel:
     
     def __str__(self) -> str:
         return f"{self.name} | {self.rating} star(s), {len(self.guests)} guest(s) staying."
+    
+    def check_in(self, guest, room_number):
+        if room_number in self.guests:
+            print("This room already is occupied.")
+        elif room_number > self.rooms:
+            print("Invalid room number. This room number doesn't exists.")
+        else:
+            self.guests[room_number] = guest
+            print(f"{guest} has checked in to room {room_number}")
