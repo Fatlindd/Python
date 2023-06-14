07a2e6b7023907eb6244c88ad62884f7d43a9cb5
Fatlind <fatlind.thaqi98@gmail.com>
2023-06-14 10:39:12 +0200

    Create mbox-short-1.py

---
+++ Code With Files/Explore With Files/mbox-short-1.py
@@ -0,0 +1,9 @@
+
+def upperLineFile(file):
+    openFile = open(file, 'rt')
+    readFile = openFile.read().strip()
+    print(readFile.upper())
+
+# 🔔 file is on Explore With Files/Files
+file = input('Enter file name: ')
+upperLineFile(file)
