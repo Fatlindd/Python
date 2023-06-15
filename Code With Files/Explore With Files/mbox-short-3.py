1fa6cf31995589caaf6a8386b8505b58688dfba7
Fatlind <fatlind.thaqi98@gmail.com>
2023-06-15 10:00:54 +0200

    Create mbox-short-3.py

---
+++ Code With Files/Explore With Files/mbox-short-3.py
@@ -0,0 +1,22 @@
+
+dict_days = dict()
+# fname = 'Files/mbox-short.txt'
+fname = input('> Give the file name: ')
+
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened: ', fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    else:
+        if words[2] not in dict_days:
+            dict_days[words[2]] = 1
+        else:
+            dict_days[words[2]] += 1
+print(dict_days)
