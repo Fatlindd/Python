24033e9f7be06905830a545932452fb2ef0ab3b8
Fatlind <fatlind.thaqi98@gmail.com>
2023-06-29 16:32:35 +0200

    Update main.py

--- Object Oriented Programming/Quiz Game Start/main.py
+++ Object Oriented Programming/Quiz Game Start/main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from data import question_data
-# from quiz_brain import QuizBrain
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -9,3 +9,7 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_questions()
+    
