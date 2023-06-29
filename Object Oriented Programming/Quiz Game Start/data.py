a2c2ab29c4792b70a507b2ce8d169fb4f4e58e71
Fatlind <fatlind.thaqi98@gmail.com>
2023-06-29 16:06:16 +0200

    Create data.py

---
+++ Object Oriented Programming/Quiz Game Start/data.py
@@ -0,0 +1,62 @@
+question_data = [
+    {
+        "text": "Python is a compiled programming language.",
+        "answer": "False"
+    },
+    {
+        "text": "Python is an object-oriented programming language.",
+        "answer": "True"
+    },
+    {
+        "text": "Python is primarily used for web development.",
+        "answer": "False"
+    },
+    {
+        "text": "Python is a statically typed language.",
+        "answer": "False"
+    },
+    {
+        "text": "Python is not suitable for scientific computing.",
+        "answer": "False"
+    },
+    {
+        "text": "Python has built-in support for regular expressions.",
+        "answer": "True"
+    },
+    {
+        "text": "Python is a platform-specific language.",
+        "answer": "False"
+    },
+    {
+        "text": "Python can only be used for small-scale projects.",
+        "answer": "False"
+    },
+    {
+        "text": "Python supports multiple programming paradigms.",
+        "answer": "True"
+    },
+    {
+        "text": "Python is not used for database management.",
+        "answer": "False"
+    },
+    {
+        "text": "Python code can be easily compiled into machine code.",
+        "answer": "False"
+    },
+    {
+        "text": "Python has a large and active open-source community.",
+        "answer": "True"
+    },
+    {
+        "text": "Python is a case-sensitive programming language.",
+        "answer": "True"
+    },
+    {
+        "text": "Python is mainly used for front-end web development.",
+        "answer": "False"
+    },
+    {
+        "text": "Python is not suitable for building mobile applications.",
+        "answer": "False"
+    }
+]
