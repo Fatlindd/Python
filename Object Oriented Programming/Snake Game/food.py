792c95485cde8cef67e714fba8cfd2b4fab6c74f
Fatlind <fatlind.thaqi98@gmail.com>
2023-07-05 11:13:31 +0200

    Update food.py

---
+++ Object Oriented Programming/Snake Game/food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("Circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("yellow")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        """ when the snake eats the food then a new position is generated for the snake's food. """
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
